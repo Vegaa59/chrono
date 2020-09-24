0bbd507518cec713ca40c8a54814d9ac76c10164
vegaa59 <sattplak@hotmail.fr>
2020-09-24 19:40:45 +0200

    compte à rebours

--- pages/compte.js
+++ pages/compte.js
@@ -1,84 +1,117 @@
-let heures = Number(prompt("combien d'heures"));
-let minutes = Number(prompt("combien de minutes"));
-let secondes = Number(prompt("combien de secondes"));
-let audio = new Audio('../son/alarm.mp3');
+let audio = new Audio("../son/alarm.mp3");
 let ol = document.createElement("ol");
+let heures = 0
+let minutes = 0
+let secondes = 0
+let timerID
+document.chronoForm.supprime.style.display = "none";
 
-window.onload = chrono();
-
-test = new Date()
-console.log(test)
-
-function chrono(){
-  if(secondes > 0) {
-       secondes-- ;
-  } else if(minutes > 0) {
-        minutes-- ;
-        secondes = 59;
-  } else if(heures > 0) {
-       heures-- ;
-        minutes = 59;
-        secondes = 59;
+onkeydown = function (e) {
+  if (e.keyCode === 32) {
+    retenir();
   }
-  if(heures <= 0 && minutes <= 0 && secondes <= 0){
-	  audio.play()
+  if (e.keyCode === 8) {
+    supprime();
+  }
+}
+
+function select_times() {
+  let select_hr = document.getElementById("select_heures").value;
+  let select_min = document.getElementById("select_minutes").value;
+  let select_sec = document.getElementById("select_secondes").value;
+  heures = select_hr;
+  minutes = select_min;
+  secondes = select_sec;
+  pause();
+  document.getElementById("selection").style.display = "none";
+}
+
+
+for (let j = 0; j < 25; j++) {
+  let option = document.createElement("option");
+  select_heures.appendChild(option);
+  option.innerHTML = j;
+}
+
+for (let k = 0; k < 60; k++) {
+  let option_minutes = document.createElement("option");
+  select_minutes.appendChild(option_minutes);
+  option_minutes.innerHTML = k;
+}
+for (let l = 0; l < 60; l++) {
+  let option_secondes = document.createElement("option");
+  select_secondes.appendChild(option_secondes);
+  option_secondes.innerHTML = l;
+}
+
+function chrono() {
+  if (secondes > 0) {
+    secondes--;
+  } else if (minutes > 0) {
+    minutes--;
+    secondes = 59;
+  } else if (heures > 0) {
+    heures--;
+    minutes = 59;
+    secondes = 59;
+  }
+  if (heures <= 0 && minutes <= 0 && secondes <= 0) {
+    audio.play();
   }
 
-  let hour = heures < 10 ? "0" + heures : heures ;
-  let min = minutes < 10 ? "0" + minutes : minutes ;
-  let sec = secondes < 10 ? "0" + secondes : secondes ;
+  let hour = heures < 10 ? "0" + heures : heures;
+  let min = minutes < 10 ? "0" + minutes : minutes;
+  let sec = secondes < 10 ? "0" + secondes : secondes;
 
-  document.getElementById("chronotime").value = hour + ":" + min + ":" + sec;
+  document.chronoForm.reset.onclick = reset;
+  document.chronoForm.supprime.onclick = supprime;
+  document.getElementById("chronotime").value = `${hour}:${min}:${sec}`;
   timerID = setTimeout("chrono()", 1000);
-  document.chronoForm.reset.onclick = reset
 }
 
-function pause(){
-      let hour = heures < 10 ? "0" + heures : heures ;
-      let min = minutes < 10 ? "0" + minutes : minutes ;
-      let sec = secondes < 10 ? "0" + secondes : secondes ;
-
-	document.chronoForm.startstop.value = "start"
-      document.chronoForm.startstop.onclick = reprendre
-      document.getElementById("chronotime").value = hour + ":" + min + ":" + sec;
-
-      clearTimeout(timerID);
+function pause() {
+  let hour = heures < 10 ? "0" + heures : heures;
+  let min = minutes < 10 ? "0" + minutes : minutes;
+  let sec = secondes < 10 ? "0" + secondes : secondes;
+  document.chronoForm.startstop.value = "start";
+  document.chronoForm.startstop.onclick = reprendre;
+  document.getElementById("chronotime").value = `${hour}:${min}:${sec}`;
+  clearTimeout(timerID);
 }
 
-function reprendre(){
-	document.chronoForm.startstop.value = "stop"
-	document.chronoForm.startstop.onclick = pause
-      document.chronoForm.reset.onclick = reset
-      chrono();
+function reprendre() {
+  document.getElementById("selection").style.display = "none";
+  document.chronoForm.startstop.value = "stop";
+  document.chronoForm.startstop.onclick = pause;
+  document.chronoForm.reset.onclick = reset;
+  chrono();
 }
 
-function reset(){
-      heures = Number(prompt("combien d'heures"));
-      minutes = Number(prompt("combien de minutes"));
-      secondes = Number(prompt("combien de secondes"));
-      let hour = heures < 10 ? "0" + heures : heures ;
-      let min = minutes < 10 ? "0" + minutes : minutes ;
-      let sec = secondes < 10 ? "0" + secondes : secondes ;
-      while (ol.firstChild) {
-		ol.removeChild(ol.firstChild);
-        }
-        document.getElementById("chronotime").value = hour + ":" + min + ":" + sec;
-        pause()
-}
-
-function retenir(){
-	let i = document.chronoForm.chronotime.value
-	document.getElementById('statretenu').appendChild(ol);
-	var li = document.createElement("li");
-	ol.appendChild(li);
-      li.innerHTML= "Temp restant : "+ i ;
-	document.chronoForm.interval.onClick = retenir
+function reset() {
+  let hour = heures < 10 ? "0" + heures : heures;
+  let min = minutes < 10 ? "0" + minutes : minutes;
+  let sec = secondes < 10 ? "0" + secondes : secondes;
+  supprime();
+  document.getElementById("chronotime").value = `${hour}:${min}:${sec}`;
+  pause();
+  document.getElementById("selection").style.display = "flex";
 }
 
 
+function retenir() {
+  document.chronoForm.supprime.style.display = "block";
+  let i = document.chronoForm.chronotime.value;
+  document.getElementById("statretenu").appendChild(ol);
+  let li = document.createElement("li");
+  ol.appendChild(li);
+  li.innerHTML = `Temp restant : ${i}`;
+  document.chronoForm.interval.onClick = retenir;
+  document.chronoForm.supprime.onclick = supprime;
+}
 
-
-
-
-
-
+function supprime() {
+  while (ol.firstChild) {
+    ol.removeChild(ol.firstChild);
+  }
+  document.chronoForm.supprime.style.display = "none";
+}
--- script.js
+++ script.js
@@ -1,81 +1,94 @@
-var startTime = 0
-var start = 0
-var end = 0
-var diff = 0
-var timerID = 0
+let startTime = 0;
+let start = 0;
+let end = 0;
+let diff = 0;
+let timerID = 0;
+document.chronoForm.supprime.style.display = "none";
 window.onload = chronoStop;
-function chrono(){
-	end = new Date()
-	diff = end - start
-	diff = new Date(diff)
-	var msec = diff.getMilliseconds()
-	var sec = diff.getSeconds()
-	var min = diff.getMinutes()
-	var hr = diff.getHours()-1
-	if (min < 10){
-		min = "0" + min
-	}
-	if (sec < 10){
-		sec = "0" + sec
-	}
-	if(msec < 10){
-		msec = "00" +msec
-	}
-	else if(msec < 100){
-		msec = "0" +msec
-	} 
-	document.getElementById("chronotime").value = hr + ":" + min + ":" + sec + ":" + msec
-	timerID = setTimeout("chrono()", 10)
-}
-function chronoStart(){
-	document.chronoForm.startstop.value = "stop"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
-	start = new Date()
-	chrono()
-}
-function chronoContinue(){
-	document.chronoForm.startstop.value = "stop"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
-	start = new Date()-diff
-	start = new Date(start)
-	chrono()
-}
-function chronoReset(){
-	document.getElementById("chronotime").value = "0:00:00:000"
-	while (ol.firstChild) {
-		ol.removeChild(ol.firstChild);
-	  }
-	start = new Date()
-}
-function chronoStopReset(){
-	document.getElementById("chronotime").value = "0:00:00:000"
-	document.chronoForm.startstop.onclick = chronoStart
-	document.chronoForm.reset.onclick = supprime
-}
-function chronoStop(){
-	document.chronoForm.startstop.value = "start"
-	document.chronoForm.startstop.onclick = chronoContinue
-	document.chronoForm.reset.onclick = chronoStopReset
-	clearTimeout(timerID)
+
+onkeydown = function (e) {
+  if (e.keyCode === 32) {
+    retenir();
+  }
+  if (e.keyCode === 8) {
+    supprime();
+  }
 }
 
-var ol = document.createElement("ol");
-
-function retenir(){
-	let i = document.chronoForm.chronotime.value
-	document.getElementById('statretenu').appendChild(ol);
-	var li = document.createElement("li");
-	ol.appendChild(li);
-    li.innerHTML= "Temp validé à : "+ i ;
-	document.chronoForm.interval.onClick = retenir
+function chrono() {
+  let end = new Date();
+  let diff = end - start;
+  diff = new Date(diff);
+  let msec = diff.getMilliseconds();
+  let sec = diff.getSeconds();
+  let min = diff.getMinutes();
+  let hr = diff.getHours() - 1;
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (msec < 10) {
+    msec = "00" + msec;
+  } else if (msec < 100) {
+    msec = "0" + msec;
+  }
+  document.getElementById("chronotime").value = `${hr}:${min}:${sec}:${msec}`;
+  timerID = setTimeout("chrono()", 10);
+  document.chronoForm.supprime.onclick = supprime;
 }
 
-
-function supprime(){
-	while (ol.firstChild) {
-		ol.removeChild(ol.firstChild);
-	  }
+function chronoStart() {
+  document.chronoForm.startstop.value = "stop";
+  document.chronoForm.startstop.onclick = chronoStop;
+  document.chronoForm.reset.onclick = chronoReset;
+  start = new Date();
+  chrono();
 }
 
+function chronoContinue() {
+  document.chronoForm.startstop.value = "stop";
+  document.chronoForm.startstop.onclick = chronoStop;
+  document.chronoForm.reset.onclick = chronoReset;
+  start = new Date() - diff;
+  start = new Date(start);
+  chrono();
+}
+
+function chronoReset() {
+  document.getElementById("chronotime").value = "0:00:00:000";
+  start = new Date();
+}
+
+function chronoStopReset() {
+  document.getElementById("chronotime").value = "0:00:00:000";
+  document.chronoForm.startstop.onclick = chronoStart;
+}
+
+function chronoStop() {
+  document.chronoForm.startstop.value = "start";
+  document.chronoForm.startstop.onclick = chronoContinue;
+  document.chronoForm.reset.onclick = chronoStopReset;
+  document.chronoForm.supprime.onclick = supprime;
+  clearTimeout(timerID);
+}
+
+let ol = document.createElement("ol");
+
+function retenir() {
+  document.chronoForm.supprime.style.display = "block";
+  let i = document.chronoForm.chronotime.value;
+  document.getElementById("statretenu").appendChild(ol);
+  let li = document.createElement("li");
+  ol.appendChild(li);
+  li.innerHTML = "Temp validé à : " + i;
+  document.chronoForm.interval.onClick = retenir;
+}
+
+function supprime() {
+  while (ol.firstChild) {
+    ol.removeChild(ol.firstChild);
+  }
+  document.chronoForm.supprime.style.display = "none";
+}
